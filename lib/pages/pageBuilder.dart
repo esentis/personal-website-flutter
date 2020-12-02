0f031ae76b6e8901fe714552b92dda058f5134b4
esentis <esentakos@yahoo.gr>
2020-12-02 20:43:07 +0200

    Adds new mobile placeholder

--- lib/pages/pageBuilder.dart
+++ lib/pages/pageBuilder.dart
@@ -2,6 +2,8 @@ import 'package:esentispws/pages/desktop/landing_page_desktop.dart';
 import 'package:flutter/material.dart';
 import 'package:responsive_builder/responsive_builder.dart';
 
+import 'mobile/landing_page_mobile.dart';
+
 class MainPage extends StatefulWidget {
   @override
   _MainPageState createState() => _MainPageState();
@@ -18,12 +20,7 @@ class ResponsiveBuilder extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ScreenTypeLayout(
-      mobile: Container(
-        width: 150,
-        height: 150,
-        color: Colors.blue,
-        child: const Text('MOBILE'),
-      ),
+      mobile: LandingPageMobile(),
       tablet: Container(
         width: 150,
         height: 150,
