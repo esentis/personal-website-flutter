fe8806907bd5fbf943280828bd588c56b0647cfc
esentis <esentakos@yahoo.gr>
2020-12-13 12:07:19 +0200

    Configures firebase

--- lib/generated_plugin_registrant.dart
+++ lib/generated_plugin_registrant.dart
@@ -5,12 +5,16 @@
 // ignore: unused_import
 import 'dart:ui';
 
+import 'package:cloud_firestore_web/cloud_firestore_web.dart';
+import 'package:firebase_core_web/firebase_core_web.dart';
 import 'package:url_launcher_web/url_launcher_web.dart';
 
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 // ignore: public_member_api_docs
 void registerPlugins(PluginRegistry registry) {
+  FirebaseFirestoreWeb.registerWith(registry.registrarFor(FirebaseFirestoreWeb));
+  FirebaseCoreWeb.registerWith(registry.registrarFor(FirebaseCoreWeb));
   UrlLauncherPlugin.registerWith(registry.registrarFor(UrlLauncherPlugin));
   registry.registerMessageHandler();
 }
