b22bc0f4fbc47f4203d95e2e3ac36f373d946491
esentis <esentakos@yahoo.gr>
2020-12-20 00:46:37 +0200

    Code refactoring

--- lib/pages/desktop/contact/contact.dart
+++ lib/pages/desktop/contact/contact.dart
@@ -1,15 +1,7 @@
+import 'package:esentispws/constants.dart';
 import 'package:flutter/material.dart';
-import 'package:url_launcher/url_launcher.dart';
 
 class ContactInfo extends StatelessWidget {
-  void launchLink(String url) async {
-    if (await canLaunch(url)) {
-      await launch(url);
-    } else {
-      throw 'Could not launch $url';
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     return Row(
