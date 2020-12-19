b6c6aa29e3d9e7bcc601b4526c6c96477c01b0c6
esentis <esentakos@yahoo.gr>
2020-12-20 00:46:09 +0200

    Adds launchLink globally

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,9 +1,18 @@
 import 'dart:ui';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 enum kLocale { greek, english }
 
 enum kTheme { light, dark }
+
+void launchLink(String url) async {
+  if (await canLaunch(url)) {
+    await launch(url);
+  } else {
+    throw 'Could not launch $url';
+  }
+}
 
 /// GREEK LANGUAGE TEXT
 
