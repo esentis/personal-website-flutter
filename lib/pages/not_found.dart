f4f7ef22aa2e304453e187f71c1e4b7f4b2b23c2
George Leonidis <esentakos@yahoo.gr>
2021-03-04 18:01:38 +0200

    Adds not found page

---
+++ lib/pages/not_found.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class PageNotFound extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        width: 250,
+        height: 250,
+        color: Colors.red,
+      ),
+    );
+  }
+}
