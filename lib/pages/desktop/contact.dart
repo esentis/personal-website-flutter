1edac7f09ac271399a585c77b791b61518ad9b13
George Leonidis <esentakos@yahoo.gr>
2021-03-10 01:24:04 +0200

    Redesigns contact buttons

--- lib/pages/desktop/contact.dart
+++ lib/pages/desktop/contact.dart
@@ -1,5 +1,20 @@
 import 'package:esentispws/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class ContactCursor extends MaterialStateMouseCursor {
+  @override
+  MouseCursor resolve(Set<MaterialState> states) {
+    if (states.contains(MaterialState.disabled)) {
+      return SystemMouseCursors.forbidden;
+    }
+    return SystemMouseCursors.click;
+  }
+
+  @override
+  String get debugDescription => 'ListTileCursor()';
+}
 
 class ContactInfo extends StatelessWidget {
   @override
@@ -8,45 +23,94 @@ class ContactInfo extends StatelessWidget {
       mainAxisAlignment: MainAxisAlignment.spaceAround,
       children: [
         Flexible(
-            child: GestureDetector(
-          onTap: () {
-            launchLink('https://www.github.com/esentis');
-          },
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Image.asset('github.png'),
-          ),
-        )),
-        Flexible(
-            child: GestureDetector(
-          onTap: () {
-            launchLink('mailto:esentakos@yahoo.gr');
-          },
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Image.asset('email.png'),
-          ),
-        )),
-        Flexible(
-          child: GestureDetector(
-            onTap: () {
-              launchLink('https://www.linkedin.com/in/gleonidis/');
+          child: ElevatedButton(
+            style: ButtonStyle(
+              mouseCursor: ContactCursor(),
+              overlayColor: MaterialStateProperty.resolveWith(
+                  (states) => Colors.white.withOpacity(0.1)),
+              backgroundColor: MaterialStateProperty.resolveWith(
+                (states) => Colors.white.withOpacity(0.1),
+              ),
+            ),
+            onPressed: () {
+              launchLink('https://www.github.com/esentis');
             },
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Image.asset('linked.png'),
+            child: const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: FaIcon(
+                FontAwesomeIcons.github,
+                size: 65,
+                color: Colors.white,
+              ),
             ),
           ),
         ),
         Flexible(
-          child: GestureDetector(
-            onTap: () {
+          child: ElevatedButton(
+            style: ButtonStyle(
+                mouseCursor: ContactCursor(),
+                overlayColor: MaterialStateProperty.resolveWith(
+                    (states) => Colors.white.withOpacity(0.1)),
+                backgroundColor: MaterialStateProperty.resolveWith(
+                  (states) => Colors.white.withOpacity(0.1),
+                )),
+            onPressed: () {
+              launchLink('mailto:esentakos@yahoo.gr');
+            },
+            child: const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: FaIcon(
+                FontAwesomeIcons.envelopeOpen,
+                size: 65,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ),
+        Flexible(
+          child: ElevatedButton(
+            style: ButtonStyle(
+              mouseCursor: ContactCursor(),
+              overlayColor: MaterialStateProperty.resolveWith(
+                  (states) => Colors.white.withOpacity(0.1)),
+              backgroundColor: MaterialStateProperty.resolveWith(
+                (states) => Colors.white.withOpacity(0.1),
+              ),
+            ),
+            onPressed: () {
+              launchLink('https://www.linkedin.com/in/gleonidis/');
+            },
+            child: const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: FaIcon(
+                FontAwesomeIcons.linkedin,
+                size: 65,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ),
+        Flexible(
+          child: ElevatedButton(
+            style: ButtonStyle(
+              mouseCursor: ContactCursor(),
+              overlayColor: MaterialStateProperty.resolveWith(
+                  (states) => Colors.white.withOpacity(0.1)),
+              backgroundColor: MaterialStateProperty.resolveWith(
+                (states) => Colors.white.withOpacity(0.1),
+              ),
+            ),
+            onPressed: () {
               launchLink(
                   'https://play.google.com/store/apps/dev?id=7040603848130357887');
             },
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Image.asset('play.png'),
+            child: const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: FaIcon(
+                FontAwesomeIcons.googlePlay,
+                size: 65,
+                color: Colors.white,
+              ),
             ),
           ),
         ),
