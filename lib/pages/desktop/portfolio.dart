4ee5af9f004abefcb7e166fda494bff7c75dbace
George Leonidis <esentakos@yahoo.gr>
2021-03-20 01:17:50 +0200

    Creates portfolio page

--- lib/pages/desktop/portfolio.dart
+++ lib/pages/desktop/portfolio.dart
@@ -1,5 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:esentispws/components/esentis_icons.dart';
 import 'package:esentispws/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:esentispws/models/project.dart';
 
 class PortfolioPage extends StatefulWidget {
   @override
@@ -18,14 +21,171 @@ class _PortfolioPageState extends State<PortfolioPage> {
             color: Colors.white,
           ),
         ),
-        Text(
-          'Coming soon...',
-          style: kStyleDefault.copyWith(
-            color: Colors.white,
-            fontSize: 17,
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.only(
+              top: 30.0,
+              bottom: 60,
+              right: 10.0,
+              left: 10,
+            ),
+            child: StreamBuilder<QuerySnapshot>(
+                stream: FirebaseFirestore.instance
+                    .collection('projects')
+                    .snapshots(),
+                builder: (context, snapshot) {
+                  if (!snapshot.hasData) {
+                    return const Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  }
+                  var projects = snapshot.data.docs.mapProjects();
+
+                  return Container(
+                    child: ListView.separated(
+                      separatorBuilder: (context, index) => Divider(
+                        color: Colors.white.withOpacity(0.5),
+                        height: 5,
+                      ),
+                      itemCount: projects.length,
+                      itemBuilder: (context, index) {
+                        var icons = skillIcons(projects[index]);
+                        return Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 9.0),
+                          child: GestureDetector(
+                            onTap: () {
+                              launchLink(projects[index].sourceUrl);
+                            },
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Flexible(
+                                  flex: 2,
+                                  child: Text(
+                                    projects[index].name,
+                                    style: const TextStyle(
+                                      fontSize: 35,
+                                    ),
+                                  ),
+                                ),
+                                Flexible(
+                                  child: Row(
+                                    children: [
+                                      ...icons.map(
+                                        (e) => Flexible(
+                                          child: Padding(
+                                            padding: const EdgeInsets.only(
+                                                right: 8.0),
+                                            child: e,
+                                          ),
+                                        ),
+                                      )
+                                    ],
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                  );
+                }),
           ),
         ),
       ],
     );
   }
+}
+
+List<Icon> skillIcons(Project project) {
+  // ignore: omit_local_variable_types
+  List<Icon> skillIcons = [];
+  project.techStack.forEach(
+    (skillName) {
+      if (skillName == 'flutter') {
+        skillIcons.add(
+          const Icon(
+            Esentis.flutter,
+            color: Color(0xff27B0EE),
+            size: 35,
+          ),
+        );
+      } else if (skillName == 'firebase') {
+        skillIcons.add(
+          const Icon(
+            Esentis.firebase,
+            color: Color(0xffFFCB2B),
+            size: 35,
+          ),
+        );
+      } else if (skillName == 'express') {
+        skillIcons.add(
+          const Icon(
+            Esentis.express,
+            color: Colors.white,
+            size: 35,
+          ),
+        );
+      } else if (skillName == 'nodejs') {
+        skillIcons.add(
+          const Icon(
+            Esentis.nodejs,
+            color: Color(0xff73B300),
+            size: 35,
+          ),
+        );
+      } else if (skillName == 'mongodb') {
+        skillIcons.add(
+          const Icon(
+            Esentis.mongodb,
+            color: Color(0xff8CCC8A),
+            size: 35,
+          ),
+        );
+      } else if (skillName == 'javascript') {
+        skillIcons.add(
+          const Icon(
+            Esentis.javascript,
+            color: Color(0xffEDD718),
+            size: 35,
+          ),
+        );
+      } else if (skillName == 'java') {
+        skillIcons.add(
+          const Icon(
+            Esentis.java,
+            color: Colors.white,
+            size: 35,
+          ),
+        );
+      } else if (skillName == 'postgres') {
+        skillIcons.add(
+          const Icon(
+            Esentis.postgresql,
+            color: Color(0xff2C618B),
+            size: 35,
+          ),
+        );
+      } else if (skillName == 'android') {
+        skillIcons.add(
+          const Icon(
+            Esentis.android,
+            color: Color(0xff73BA58),
+            size: 35,
+          ),
+        );
+      } else if (skillName == 'apple') {
+        skillIcons.add(
+          const Icon(
+            Esentis.apple,
+            color: Color(0xffB0B1B6),
+            size: 35,
+          ),
+        );
+      }
+    },
+  );
+  return skillIcons;
 }
