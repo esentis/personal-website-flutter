4d852ddb00a1797f2c52199c1f042b08d94ed125
esentis <esentakos@yahoo.gr>
2022-04-21 23:57:09 +0300

    💄 Reworks portfolio widget

--- lib/pages/desktop/portfolio.dart
+++ lib/pages/desktop/portfolio.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:date_format/date_format.dart';
 import 'package:esentispws/components/esentis_icons.dart';
@@ -16,7 +18,12 @@ class PortfolioPage extends StatefulWidget {
 }
 
 class _PortfolioPageState extends State<PortfolioPage> {
-  final ScrollController _scrollController = ScrollController();
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return StreamBuilder<QuerySnapshot>(
@@ -34,86 +41,77 @@ class _PortfolioPageState extends State<PortfolioPage> {
             (a, b) => b.createdAt!.millisecondsSinceEpoch
                 .compareTo(a.createdAt!.millisecondsSinceEpoch),
           );
-        return RawScrollbar(
-          isAlwaysShown: true,
-          controller: _scrollController,
-          thumbColor: Colors.white,
-          radius: const Radius.circular(20),
-          thickness: 10,
-          child: GridView.builder(
-            controller: _scrollController,
-            gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-              maxCrossAxisExtent: widget.deviceType == DeviceType.mobile ||
-                      widget.deviceType == DeviceType.tablet
-                  ? MediaQuery.of(context).size.width
-                  : MediaQuery.of(context).size.width / 2,
-              mainAxisExtent: 400,
-              crossAxisSpacing: 5,
-              mainAxisSpacing: 5,
-            ),
-            itemCount: projects.length,
-            padding: EdgeInsets.zero,
-            itemBuilder: (context, index) {
-              final icons = skillIcons(projects[index]);
-              return Padding(
-                padding: const EdgeInsets.all(14.0),
-                child: Card(
-                  child: Padding(
-                    padding: const EdgeInsets.all(20.0),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        kSelectableText(
-                          text: projects[index].name!,
-                          fontSize: 25,
-                          fontWeight: FontWeight.bold,
-                          color: kColorBackground,
-                        ),
-                        const SizedBox(
-                          height: 20,
-                        ),
-                        kSelectableText(
-                          text: projects[index].description!,
-                          fontSize: 20,
-                          color: kColorBackground,
-                        ),
-                        const SizedBox(
-                          height: 20,
-                        ),
-                        TextButton(
-                          style: ButtonStyle(
-                            padding: MaterialStateProperty.resolveWith(
-                              (states) => const EdgeInsets.all(20),
-                            ),
-                            backgroundColor: MaterialStateProperty.resolveWith(
-                              (states) => kColorBackground,
-                            ),
+        return ListView.builder(
+          itemCount: projects.length,
+          scrollDirection: Axis.horizontal,
+          physics: const BouncingScrollPhysics(
+            parent: AlwaysScrollableScrollPhysics(),
+          ),
+          itemBuilder: (context, index) {
+            final icons = skillIcons(projects[index]);
+
+            return Center(
+              child: Padding(
+                padding: EdgeInsets.all(
+                  widget.deviceType == DeviceType.mobile
+                      ? 18
+                      : widget.deviceType == DeviceType.tablet
+                          ? 25
+                          : 30,
+                ),
+                child: Container(
+                  width: 350,
+                  height: 400,
+                  decoration: BoxDecoration(
+                    color: kColorBackground,
+                    borderRadius: BorderRadius.circular(12),
+                    border: Border.all(color: Colors.transparent),
+                    boxShadow: [
+                      BoxShadow(
+                        blurRadius: 1,
+                        color: Colors.black.withOpacity(0.3),
+                        offset: const Offset(1, 1),
+                        spreadRadius: 1,
+                      ),
+                      BoxShadow(
+                        blurRadius: 1,
+                        color: Colors.white.withOpacity(0.3),
+                        offset: const Offset(-1, -1),
+                        spreadRadius: 1,
+                      )
+                    ],
+                  ),
+                  child: Column(
+                    children: [
+                      Wrap(
+                        children: icons
+                            .map(
+                              (e) => Padding(
+                                padding: const EdgeInsets.all(15),
+                                child: e,
+                              ),
+                            )
+                            .toList(),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Text(
+                        projects[index].description!,
+                        style: kStyleDefault,
+                        textAlign: TextAlign.center,
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      Column(
+                        children: [
+                          Text(
+                            'Release date',
+                            style: kStyleDefault,
                           ),
-                          onPressed: () {
-                            launchLink(projects[index].sourceUrl!);
-                          },
-                          child: kText(
-                            text: 'Source Code',
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20,
-                          ),
-                        ),
-                        const SizedBox(
-                          height: 20,
-                        ),
-                        Flexible(
-                          child: Padding(
-                            padding: const EdgeInsets.fromLTRB(0, 14, 8, 8),
-                            child: Wrap(
-                              children: icons,
-                            ),
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.fromLTRB(0, 8, 8, 8),
-                          child: kText(
-                            text: formatDate(
+                          Text(
+                            formatDate(
                               DateTime.fromMillisecondsSinceEpoch(
                                 projects[index]
                                     .createdAt!
@@ -121,24 +119,139 @@ class _PortfolioPageState extends State<PortfolioPage> {
                               ),
                               [d, '-', MM, '-', yyyy],
                             ),
-                            fontSize: 15,
-                            color: kColorBackground,
+                            style: kStyleDefault,
+                          ),
+                        ],
+                      ),
+                      TextButton(
+                        onPressed: () {
+                          launchLink(projects[index].sourceUrl!);
+                        },
+                        child: Text(
+                          'Source code',
+                          style: kStyleDefault.copyWith(
+                            fontSize: 20,
+                            color: kColorHomeBackground,
                           ),
                         ),
-                      ],
-                    ),
+                      )
+                    ],
                   ),
                 ),
-              );
-            },
-          ),
+              ),
+            );
+          },
         );
+        // return RawScrollbar(
+        //   isAlwaysShown: true,
+        //   controller: _scrollController,
+        //   thumbColor: Colors.white,
+        //   radius: const Radius.circular(20),
+        //   thickness: 10,
+        //   child: GridView.builder(
+        //     controller: _scrollController,
+        //     gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+        //       maxCrossAxisExtent: widget.deviceType == DeviceType.mobile ||
+        //               widget.deviceType == DeviceType.tablet
+        //           ? MediaQuery.of(context).size.width
+        //           : MediaQuery.of(context).size.width / 2,
+        //       mainAxisExtent: 400,
+        //       crossAxisSpacing: 5,
+        //       mainAxisSpacing: 5,
+        //     ),
+        //     itemCount: projects.length,
+        //     padding: EdgeInsets.zero,
+        //     itemBuilder: (context, index) {
+        //       final icons = skillIcons(projects[index]);
+        //       return Padding(
+        //         padding: const EdgeInsets.all(14.0),
+        //         child: Card(
+        //           child: Padding(
+        //             padding: const EdgeInsets.all(20.0),
+        //             child: Column(
+        //               mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        //               crossAxisAlignment: CrossAxisAlignment.start,
+        //               children: [
+        //                 kSelectableText(
+        //                   text: projects[index].name!,
+        //                   fontSize: 25,
+        //                   fontWeight: FontWeight.bold,
+        //                   color: kColorBackground,
+        //                 ),
+        //                 const SizedBox(
+        //                   height: 20,
+        //                 ),
+        //                 kSelectableText(
+        //                   text: projects[index].description!,
+        //                   fontSize: 20,
+        //                   color: kColorBackground,
+        //                 ),
+        //                 const SizedBox(
+        //                   height: 20,
+        //                 ),
+        //                 TextButton(
+        //                   style: ButtonStyle(
+        //                     padding: MaterialStateProperty.resolveWith(
+        //                       (states) => const EdgeInsets.all(20),
+        //                     ),
+        //                     backgroundColor: MaterialStateProperty.resolveWith(
+        //                       (states) => kColorBackground,
+        //                     ),
+        //                   ),
+        //                   onPressed: () {
+        //                     launchLink(projects[index].sourceUrl!);
+        //                   },
+        //                   child: kText(
+        //                     text: 'Source Code',
+        //                     fontWeight: FontWeight.bold,
+        //                     fontSize: 20,
+        //                   ),
+        //                 ),
+        //                 const SizedBox(
+        //                   height: 20,
+        //                 ),
+        //                 Flexible(
+        //                   child: Padding(
+        //                     padding: const EdgeInsets.fromLTRB(0, 14, 8, 8),
+        //                     child: Wrap(
+        //                       children: icons,
+        //                     ),
+        //                   ),
+        //                 ),
+        //                 Flexible(
+        //                   child: Padding(
+        //                     padding: const EdgeInsets.fromLTRB(0, 8, 8, 8),
+        //                     child: kText(
+        // text: formatDate(
+        //   DateTime.fromMillisecondsSinceEpoch(
+        //     projects[index]
+        //         .createdAt!
+        //         .millisecondsSinceEpoch,
+        //   ),
+        //   [d, '-', MM, '-', yyyy],
+        // ),
+        //                       fontSize: 15,
+        //                       color: kColorBackground,
+        //                     ),
+        //                   ),
+        //                 ),
+        //               ],
+        //             ),
+        //           ),
+        //         ),
+        //       );
+        //     },
+        //   ),
+        // );
       },
     );
   }
 }
 
 List<Icon> skillIcons(Project project) {
+  final double iconSize = 45;
+
+  final EdgeInsets padding = EdgeInsets.all(20);
   // ignore: omit_local_variable_types
   final List<Icon> skillIcons = [];
   // ignore: avoid_function_literals_in_foreach_calls
@@ -146,170 +259,170 @@ List<Icon> skillIcons(Project project) {
     (skillName) {
       if (skillName == 'flutter') {
         skillIcons.add(
-          const Icon(
+          Icon(
             Esentis.flutter,
             color: Color(0xff27B0EE),
-            size: 30,
+            size: iconSize,
           ),
         );
       } else if (skillName == 'firebase') {
         skillIcons.add(
-          const Icon(
+          Icon(
             Esentis.firebase,
             color: Color(0xffFFCB2B),
-            size: 30,
+            size: iconSize,
           ),
         );
       } else if (skillName == 'express') {
         skillIcons.add(
-          const Icon(
+          Icon(
             Esentis.express,
             color: Colors.black,
-            size: 30,
+            size: iconSize,
           ),
         );
       } else if (skillName == 'nodejs') {
         skillIcons.add(
-          const Icon(
+          Icon(
             Esentis.nodejs,
             color: Color(0xff73B300),
-            size: 30,
+            size: iconSize,
           ),
         );
       } else if (skillName == 'mongodb') {
         skillIcons.add(
-          const Icon(
+          Icon(
             Esentis.mongodb,
             color: Color(0xff8CCC8A),
-            size: 30,
+            size: iconSize,
           ),
         );
       } else if (skillName == 'javascript') {
         skillIcons.add(
-          const Icon(
+          Icon(
             Esentis.javascript,
             color: Color(0xffEDD718),
-            size: 30,
+            size: iconSize,
           ),
         );
       } else if (skillName == 'java') {
         skillIcons.add(
-          const Icon(
+          Icon(
             Esentis.java,
             color: Color(0xff0776BD),
-            size: 30,
+            size: iconSize,
           ),
         );
       } else if (skillName == 'postgres') {
         skillIcons.add(
-          const Icon(
+          Icon(
             Esentis.postgresql,
             color: Color(0xff2C618B),
-            size: 30,
+            size: iconSize,
           ),
         );
       } else if (skillName == 'android') {
         skillIcons.add(
-          const Icon(
+          Icon(
             Esentis.android,
             color: Color(0xff73BA58),
-            size: 30,
+            size: iconSize,
           ),
         );
       } else if (skillName == 'apple') {
         skillIcons.add(
-          const Icon(
+          Icon(
             Esentis.apple,
             color: Color(0xffB0B1B6),
-            size: 30,
+            size: iconSize,
           ),
         );
       } else if (skillName == 'python') {
         skillIcons.add(
-          const Icon(
+          Icon(
             Esentis.python,
             color: Color(0xff5E7D98),
-            size: 30,
+            size: iconSize,
           ),
         );
       } else if (skillName == 'csharp') {
         skillIcons.add(
-          const Icon(
+          Icon(
             Esentis.csharp,
             color: Color(0xff642076),
-            size: 30,
+            size: iconSize,
           ),
         );
       } else if (skillName == 'css') {
         skillIcons.add(
-          const Icon(
+          Icon(
             Esentis.css3,
             color: Color(0xff1C89B6),
-            size: 30,
+            size: iconSize,
           ),
         );
       } else if (skillName == 'react') {
         skillIcons.add(
-          const Icon(
+          Icon(
             Esentis.react,
             color: Color(0xff58C4E9),
-            size: 30,
+            size: iconSize,
           ),
         );
       } else if (skillName == 'dotnet') {
         skillIcons.add(
-          const Icon(
+          Icon(
             Esentis.dotnet,
             color: Color(0xff47B6E7),
-            size: 30,
+            size: iconSize,
           ),
         );
       } else if (skillName == 'bootstrap') {
         skillIcons.add(
-          const Icon(
+          Icon(
             Esentis.bootstrap,
             color: Color(0xff7511F1),
-            size: 30,
+            size: iconSize,
           ),
         );
       } else if (skillName == 'jquery') {
         skillIcons.add(
-          const Icon(
+          Icon(
             Esentis.jquery,
             color: Color(0xff345975),
-            size: 30,
+            size: iconSize,
           ),
         );
       } else if (skillName == 'html') {
         skillIcons.add(
-          const Icon(
+          Icon(
             Esentis.html5,
             color: Color(0xffEA6D2C),
-            size: 30,
+            size: iconSize,
           ),
         );
       } else if (skillName == 'studio') {
         skillIcons.add(
-          const Icon(
+          Icon(
             Esentis.visualstudio,
             color: Color(0xffB482EA),
-            size: 30,
+            size: iconSize,
           ),
         );
       } else if (skillName == 'dart') {
         skillIcons.add(
-          const Icon(
+          Icon(
             Esentis.dart,
             color: Color(0xff75ACE6),
-            size: 30,
+            size: iconSize,
           ),
         );
       } else if (skillName == 'dotnetcore') {
         skillIcons.add(
-          const Icon(
+          Icon(
             Esentis.dotnetcore,
             color: Color(0xff694097),
-            size: 30,
+            size: iconSize,
           ),
         );
       }
