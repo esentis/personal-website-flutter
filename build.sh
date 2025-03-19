a6230884dacd531fab487d64f40d00a570158d06
esentis <52862600+esentis@users.noreply.github.com>
2025-03-19 23:45:15 +0200

    ⬆️ Enhance build script with cleanup, dependency retrieval, and deployment steps

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
+echo "Cleaning up the build folder"
 fvm flutter clean
+echo "Getting the dependencies"
 fvm flutter pub get
 clear
+echo "Building the web app"
 fvm flutter build web --release --web-renderer=canvaskit
+echo "Deploying the web app"
 firebase deploy
